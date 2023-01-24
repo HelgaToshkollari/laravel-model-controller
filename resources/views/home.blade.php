5a57e1a3239889b6a96cad78f0a8cab0d24a57be
HelgaToshkollari <toshkollari.helga@gmail.com>
2023-01-24 10:43:41 +0100

    adds changes

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section("main-content")
+
+
+@foreach ($movies as $movie )
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{$movie["title"]}}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">{{$movie["original_title"]}}</h6>
+          <p class="card-text">{{$movie["nationality"]}}</p>
+          <p class="card-text">{{$movie["date"]}}</p>
+          <p class="card-text">{{$movie["vote"]}}</p>
+        </div>
+     </div>
+   
+@endforeach
+
+
+
+@endsection
